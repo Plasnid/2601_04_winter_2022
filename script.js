018ca2d1bcfbfc27c60d3b9e1ea99a96b314e812
Plasnid <stuart@sackler.ca>
2022-02-02 15:29:07 -0500

    2601-04 Winter 2022 demo

---
+++ script.js
@@ -0,0 +1,44 @@
+/**
+ * * START
+ * * PROGRAM getTheBall
+ * * Create variable isHandOpen
+ * * Create variable elbowAngle
+ * * Set isHandOpen to true
+ * * Set elbowAngle to 0
+ * 
+ * * Set elbowAngle to 105
+ * * Move elbowjoint to elbowAngle
+ * * Set isHandOpen to false
+ * * Set elbowAngle to 0
+ * * Move elbowJoint to elboAngle
+ * * END
+ */
+
+// ** Strings, numbers, booleans, array, object
+
+// ** let and const
+
+let faveFood1 = "Biryani";
+console.log(faveFood1);
+let faveFood2 = 'Pastizzi';
+console.log(faveFood2);
+
+// * template literal
+let foodsToTry = `You should try ${faveFood1} and ${faveFood2} they are delicious!`;
+console.log(foodsToTry);
+
+// * Booleans
+let dinosaursDontExistAnymore = 1;
+let earthIsFlat = 0;
+
+// * numbers
+// * 5 is an integer
+// * 3.14159 is a float
+let myFaveInteger = 5;
+let myFaveFloat = 3.14;
+
+let strangeCarSetupWheels = "3";
+console.log(typeof(strangeCarSetupWheels));
+console.log(typeof(parseInt(strangeCarSetupWheels)));
+
+
